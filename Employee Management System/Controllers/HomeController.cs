f303d7006ecef7a2fe3758b6da17d02e18b658ed
Rohit-Jarad <rohitjarad007@gmial.com>
2026-01-08 08:56:50 +0530

    Remove Privacy from fotter

--- Employee Management System/Controllers/HomeController.cs
+++ Employee Management System/Controllers/HomeController.cs
@@ -23,10 +23,6 @@ namespace Employee_Management_System.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
